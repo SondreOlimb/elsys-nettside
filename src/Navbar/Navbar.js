ae28ce0fd82d8951bd8f662a9e377c5f0a433466
SondreOlimb <sondre.olimb@gmail.com>
2020-02-01 19:44:08 +0100

    stadard profilbilde

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,19 +1,22 @@
 import React from "react";
-import logo from "../Bilder/bird-logos-png.png";
+import logo from "../Bilder/profile.jpg";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import firebase from "firebase";
 
 function Navbar() {
+  var img = new Image();
+
+  if (firebase.auth().currentUser.photoURL) {
+    img.src = firebase.auth().currentUser.photoURL;
+  } else {
+    img.src = logo;
+  }
   return (
     <div className="Flex-navbar">
       <div className="items-container">
         <Link to="/" className="logolink">
-          <img
-            alt="pic"
-            src={firebase.auth().currentUser.photoURL}
-            className="pic"
-          />
+          <img alt="pic" src={img.src} className="pic" />
         </Link>
 
         <p className="user"> {firebase.auth().currentUser.displayName}</p>
