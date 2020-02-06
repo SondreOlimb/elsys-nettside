7d0720177f15a83692a22bce0ace897ad366b958
SondreOlimb <sondre.olimb@gmail.com>
2020-02-06 10:42:45 +0100

    tester

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -13,7 +13,7 @@ function Data() {
         <img className="pic" src={Chart} alt="" />
       </div>
       <div className="Card">
-        Data <p> Her står det noe data</p>
+        Data <p> Her noe data</p>
       </div>
       <div className="Card">
         Data <p> Her står det noe data</p>
