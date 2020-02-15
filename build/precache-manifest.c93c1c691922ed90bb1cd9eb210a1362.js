e19aa847b153ddbb5aac08c12107f60c8ddaca51
SondreOlimb <sondre.olimb@gmail.com>
2020-02-15 21:39:27 +0100

    heatmap2

---
+++ build/precache-manifest.c93c1c691922ed90bb1cd9eb210a1362.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb14a412090e903f74dac770d8913ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "017c5be48174d803985e",
+    "url": "/static/css/main.d9e359dc.chunk.css"
+  },
+  {
+    "revision": "96080213f08b7be6b2d2",
+    "url": "/static/js/2.281364c0.chunk.js"
+  },
+  {
+    "revision": "47d5ece052b90d87662d53f677cc0ed4",
+    "url": "/static/js/2.281364c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "017c5be48174d803985e",
+    "url": "/static/js/main.4082265a.chunk.js"
+  },
+  {
+    "revision": "4e66a78ae339d41908a0",
+    "url": "/static/js/runtime-main.dc119717.js"
+  },
+  {
+    "revision": "57c4a2efac1d479978ac839d3600c602",
+    "url": "/static/media/bird-logos-png.57c4a2ef.png"
+  },
+  {
+    "revision": "f00ade2c0f4c043f02a4b6d2c0bacbf7",
+    "url": "/static/media/elsys.f00ade2c.png"
+  },
+  {
+    "revision": "5e6b5f9dbd9895e40ac2440a154504f0",
+    "url": "/static/media/logo_stor.5e6b5f9d.svg"
+  },
+  {
+    "revision": "39eb0bbf2cc33ba02f53f8585004f820",
+    "url": "/static/media/profile.39eb0bbf.jpg"
+  }
+]);
