8d6be9006e27a7f95466af376db245a1e708ca8c
SondreOlimb <sondre.olimb@gmail.com>
2020-02-15 22:32:40 +0100

    heatmap trondheim

--- src/Components/Maps/Maps.js
+++ src/Components/Maps/Maps.js
@@ -1,22 +1,24 @@
 import React, { PureComponent, useState } from "react";
 import { render } from "react-dom";
-import { StaticMap, NavigationControl } from "react-map-gl";
+import { StaticMap, NavigationControl, GeolocateControl } from "react-map-gl";
+import { MapController } from "deck.gl";
 
 import DeckGL from "@deck.gl/react";
 import { HeatmapLayer } from "@deck.gl/aggregation-layers";
 import "./Maps.scss";
+import { mapData } from "./MapData";
 // Set your mapbox token here
 const MAPBOX_TOKEN =
   "pk.eyJ1Ijoib2xpbWIiLCJhIjoiY2s2NndxaG05MDJkajNqc2RoZDY4bjhjcyJ9.qYYAaBGI80WGqTHL6NrP5A"; // eslint-disable-line
-const DATA_URL =
-  "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json"; // eslint-disable-line
+const DATA_URL = mapData;
+//"https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json"; // eslint-disable-line
 
 const INITIAL_VIEW_STATE = {
   width: 10,
   height: 10,
-  longitude: -73.75,
-  latitude: 40.73,
-  zoom: 9,
+  longitude: 10.4,
+  latitude: 63.41,
+  zoom: 12,
   maxZoom: 16,
   pitch: 0,
   bearing: 0
@@ -56,7 +58,7 @@ export default class Maps extends PureComponent {
           className="Maps"
           //className="deck"
           initialViewState={INITIAL_VIEW_STATE}
-          controller={true}
+          controller={{ type: MapController, dragRotate: false }}
           layers={this._renderLayers()}
           height="100%"
           width="100%"
@@ -68,7 +70,7 @@ export default class Maps extends PureComponent {
             preventStyleDiffing={true}
             mapboxApiAccessToken={MAPBOX_TOKEN}
             position="absolute"
-          />
+          ></StaticMap>
         </DeckGL>
       </div>
     );
