d9b165a8934a87c9fa089182647c1c5443a12c49
SondreOlimb <sondre.olimb@gmail.com>
2020-02-16 15:41:02 +0100

    nytt heatmap

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c93c1c691922ed90bb1cd9eb210a1362.js"
+  "/precache-manifest.ace0e9d60e9803ec966e7c0c427ffbb5.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/Components/Maps/Maps.js
+++ src/Components/Maps/Maps.js
@@ -1,38 +1,44 @@
-import React, { PureComponent, useState } from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
-import { StaticMap, NavigationControl, GeolocateControl } from "react-map-gl";
-import { MapController } from "deck.gl";
-
-import DeckGL from "@deck.gl/react";
-import { HeatmapLayer } from "@deck.gl/aggregation-layers";
+import DeckGL, { ArcLayer, HeatmapLayer } from "deck.gl";
+import ReactMapGL, {
+  NavigationControl,
+  GeolocateControl,
+  FullscreenControl
+} from "react-map-gl";
 import "./Maps.scss";
 import { mapData } from "./MapData";
-// Set your mapbox token here
-const MAPBOX_TOKEN =
-  "pk.eyJ1Ijoib2xpbWIiLCJhIjoiY2s2NndxaG05MDJkajNqc2RoZDY4bjhjcyJ9.qYYAaBGI80WGqTHL6NrP5A"; // eslint-disable-line
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const TOKEN =
+  "pk.eyJ1Ijoib2xpbWIiLCJhIjoiY2s2NndxaG05MDJkajNqc2RoZDY4bjhjcyJ9.qYYAaBGI80WGqTHL6NrP5A"; // Set your mapbox token here
 const DATA_URL = mapData;
-//"https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json"; // eslint-disable-line
 
-const INITIAL_VIEW_STATE = {
-  width: 10,
-  height: 10,
-  longitude: 10.4,
-  latitude: 63.41,
-  zoom: 12,
-  maxZoom: 16,
-  pitch: 0,
-  bearing: 0
+const geolocateStyle = {
+  float: "left",
+  margin: "50px",
+  padding: "10px"
 };
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        longitude: 10.4035,
+        latitude: 63.4135,
+        zoom: 11,
+        bearing: 0,
+        pitch: 30
+      }
+    };
+  }
 
-//const view = new View({ width: "50%", height: "50%" });
-
-export default class Maps extends PureComponent {
   _renderLayers() {
     const {
       data = DATA_URL,
       intensity = 1,
       threshold = 0.03,
-      radiusPixels = 30
+      radiusPixels = 50
     } = this.props;
 
     return [
@@ -49,34 +55,41 @@ export default class Maps extends PureComponent {
     ];
   }
 
+  _onViewportChange = viewport => {
+    this.setState({ viewport });
+  };
+
   render() {
+    const { viewport } = this.state;
     const { mapStyle = "mapbox://styles/mapbox/dark-v9" } = this.props;
 
     return (
-      <div className="Maps" style={{ position: "relative" }}>
-        <DeckGL
-          className="Maps"
-          //className="deck"
-          initialViewState={INITIAL_VIEW_STATE}
-          controller={{ type: MapController, dragRotate: false }}
-          layers={this._renderLayers()}
-          height="100%"
+      <div className="Maps">
+        <ReactMapGL
+          {...viewport}
           width="100%"
-          position="relative"
+          height="90vh"
+          maxPitch={85}
+          onViewportChange={this._onViewportChange}
+          mapboxApiAccessToken={TOKEN}
+          mapStyle={mapStyle}
         >
-          <StaticMap
-            reuseMaps
-            mapStyle={mapStyle}
-            preventStyleDiffing={true}
-            mapboxApiAccessToken={MAPBOX_TOKEN}
-            position="absolute"
-          ></StaticMap>
-        </DeckGL>
+          <DeckGL viewState={viewport} layers={this._renderLayers()} />
+          <div style={{ position: "absolute", right: 0 }}>
+            <NavigationControl visualizePitch={true} />
+            <FullscreenControl container={document.querySelector("body")} />
+            <GeolocateControl
+              //style={geolocateStyle}
+              positionOptions={{ enableHighAccuracy: true }}
+              trackUserLocation={true}
+            />
+          </div>
+        </ReactMapGL>
       </div>
     );
   }
 }
 
-export function renderToDOM(container) {
-  render(<Maps />, container);
+export function renderToDom(container) {
+  render(<App />, container);
 }
